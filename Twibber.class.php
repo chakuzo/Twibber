53d28be8260788d920c5a3bcc1b6ffd838233c3a
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2010-12-28 17:48:42 +0000

    Schon etwas angefangen, kp obs geht.... Bin erstmal zocken
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@9 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- Twibber.class.php
+++ Twibber.class.php
@@ -7,26 +7,57 @@ define("mysql_pw","password"); // The Password for the User
 define("mysql_db","database"); // The Database Name
 define("mysql_local","localhost"); // I think you shouldnt change this..
 
+/* Fill out the Data for the connection to read out the WCF Data,
+ * if it's the same as the above database connection, you can leave these here
+ * empty.
+ */
+define("mysql_user_wcf","");
+define("mysql_pw_wcf","");
+define("mysql_db_wcf","");
+define("mysql_local_wcf","");
+define("wcf_name_prefix","WCF1_"); // The prefix which is used for the wcf !!!
+				    // Provide it with syntax "wcfX_", X as a number !!!
+if(wcf_name_prefix == "WCF1_"){
+    Throw new Exception("ERROR! Bitte füllen sie Linie 17 in Twibber.class.php richtig aus.");
+}
+
+$mysqli = new mysqli(mysql_local,mysql_user,mysql_pw,mysql_db);
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+
 /**
- * Description of Twibber
+ * Classes for Twibber
  *
  * @author Kurt
  */
 class Twibber {
-    function fetchTwibber($latest = true,$global = false,$nick = ''){
+    function fetchTwibber($latest = true, $global = false, $nick = ''){
 	// @TODO Function to fetch the Twibber statuses.
     }
-    function createTwibber($message,$usernick){
+    function createTwibber($message, $usernick){
 	// @TODO Post new twibber.
+	$mysqli->query("INSERT INTO `twibber_entry`(`nickname`,`text`,`date`) VALUES('".$usernick."','".$message."','".date("H:m:s d.m.Y")."')");
     }
 }
 class wcf{
-    function getData(){
+    function getData($Data){
 	// @TODO Read the database from wcf sha1($salt.sha1($salt.$password));
+	if((mysql_user_wcf == "" && mysql_pw_wcf == "" && mysql_db_wcf == "" && mysql_local_wcf == "")&& (mysql_user != "" && mysql_pw != "" && mysql_db != "" && mysql_local != "")){
+	    $result = $mysqli->query("SELECT * FROM `".wcf_name_prefix."` ".$Data);
+	    return $result->fetch_assoc();
+	}elseif(mysql_user_wcf != "" && mysql_pw_wcf != "" && mysql_db_wcf != "" && mysql_local_wcf != ""){
+	    $this->mysqli(mysql_user_wcf, mysql_pw_wcf, mysql_db_wcf, mysql_local_wcf);
+	    $mysqli2->query("SELECT * FROM `".wcf_name_prefix."` ".$Data);
+	    return $result->fetch_assoc();
+	}
+    }
+    function mysqli($user,$pw,$db,$local){
+	$mysqli2 = new mysqli($user,$pw,$db,$local);
     }
 }
 
-$mysqli = new mysqli();
 $Twibber = new Twibber();
 $wcf = new wcf();
 
