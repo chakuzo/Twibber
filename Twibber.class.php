e29277c8e5415a3c33ca017fb9a2db2918da7577
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2010-12-28 16:38:01 +0000

    Constants mysql_user, mysql_pw, mysql_db & mysql_local now are used. Maybe chakuzo doenst want us to know the pw? :P
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@8 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- Twibber.class.php
+++ Twibber.class.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Fill out the Data to the connection.
+ */
+define("mysql_user",""); // The User for the Database
+define("mysql_pw","password"); // The Password for the User
+define("mysql_db","database"); // The Database Name
+define("mysql_local","localhost"); // I think you shouldnt change this..
 
 /**
  * Description of Twibber
@@ -21,5 +28,6 @@ class wcf{
 
 $mysqli = new mysqli();
 $Twibber = new Twibber();
+$wcf = new wcf();
 
 ?>
