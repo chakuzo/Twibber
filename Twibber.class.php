84893b5919502d5ceb22f2c0c210d44afff7b72f
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2010-12-28 11:31:40 +0000

    git-svn-id: http://twbbler.googlecode.com/svn/trunk@2 1a2ef0f9-509c-9fab-45e8-d1e93232252b

---
+++ Twibber.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of Twibber
+ *
+ * @author Kurt
+ */
+class Twibber {
+    function fetchTwibber($latest = true,$global = false,$nick = ''){
+	// @TODO Function to fetch the Twibber statuses.
+    }
+}
+
+$mysqli = new mysqli();
+$Twibber = new Twibber();
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>TWBBler - Das WBB MicroBlogging Social Network</title>
+	<link rel="stylesheet" href="style.css"> 
+    </head>
+    <body>
+	<header></header>
+	<?php
+	$Twibber->fetchTwibber(true,true);
+	?>
+	<footer><a href="http://www.pixelio.de">M. Großmann / pixelio.de</a></footer>
+    </body>
+</html>
