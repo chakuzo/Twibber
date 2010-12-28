f88f337aeade158b44fc6ffeae783bd7394192cd
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2010-12-28 14:52:23 +0000

    Ahh deine Mudda.. Achja Phineas & Ferb :D
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@4 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- Twibber.class.php
+++ Twibber.class.php
@@ -9,6 +9,9 @@ class Twibber {
     function fetchTwibber($latest = true,$global = false,$nick = ''){
 	// @TODO Function to fetch the Twibber statuses.
     }
+    function createTwibber($message,$usernick){
+	// @TODO Post new twibber.
+    }
 }
 
 $mysqli = new mysqli();
