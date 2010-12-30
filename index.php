030991859cf230f40425ef115878c9afab9a5d96
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2010-12-30 11:33:00 +0000

    Updates#
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@48 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ include("Twibber.class.php");
         <title>TWiBBer - Das WBB MicroBlogging Social Network</title>
 	<link rel="stylesheet" href="style.css" type="text/css"> 
 	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'> 
+	<link href='http://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
     </head>
     <body>
 	<header>TwBBs || <a href="http://wbblite2.de">WBBLite2.de</a></header>
-	<div id="logo"><img src="res/img/logo.png" /></div>
+	<div id="logo"><a href="index.php"><img src="res/img/logo.png"></a></div>
 	<div id="post_it">
 	    <h3>Phineas, was machst du grade?</h3>
 	    <div id="status"></div>
-	    <label for="nickname">Nickname: </label><input type="text" id="nickname">
+	    <label for="nickname" id="label_nick">Nickname: </label><input type="text" id="nickname">
 	    <textarea id="input_text" maxlength="250" autofocus placeholder="Schreib deinen Freunden hier, was du gerade tust."></textarea>
 	    <br><div class="right"><label for="input_text">0 Zeichen</label><button id="twibber_it" onclick="dyn_submit();">Twibbern</button></div>
 	<?php
