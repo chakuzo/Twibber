3cc7fbb418485e44d6702a7f977e194955634813
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2011-01-01 17:39:57 +0000

    added better upgrade support
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@87 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- Twibber.class.php
+++ Twibber.class.php
@@ -1,21 +1,6 @@
 <?php
 include("StringUtil.class.php");
-/**
- * Fill out the Data to the connection.
- */
-define("mysql_user",""); // The User for the Database
-define("mysql_pw","password"); // The Password for the User
-define("mysql_db","database"); // The Database Name
-define("mysql_local","localhost"); // I think you shouldnt change this..
-define("wcf_name_prefix","WCF1_"); // The prefix which is used for the wcf !!!
-				    // Provide it with syntax "wcfX_", X as a number !!!
-/**
- * Fill out the Data for the connection to read out the WCF Data,
- */
-define("mysql_user_wcf","");
-define("mysql_pw_wcf","");
-define("mysql_db_wcf","");
-define("mysql_local_wcf","localhost");
+include("config.inc.php");
 
 if(wcf_name_prefix == "WCF1_"){
     die("ERROR! Bitte füllen sie Linie 10 in Twibber.class.php richtig aus.");
---
+++ config.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Fill out the Data to the connection.
+ */
+define("mysql_user",""); // The User for the Database
+define("mysql_pw","password"); // The Password for the User
+define("mysql_db","database"); // The Database Name
+define("mysql_local","localhost"); // I think you shouldnt change this..
+define("wcf_name_prefix","WCF1_"); // The prefix which is used for the wcf !!!
+				    // Provide it with syntax "wcfX_", X as a number !!!
+/**
+ * Fill out the Data for the connection to read out the WCF Data,
+ */
+define("mysql_user_wcf","");
+define("mysql_pw_wcf","");
+define("mysql_db_wcf","");
+define("mysql_local_wcf","localhost");
+
+/*
+ * Examples:
+ * define("wcf_name_prefix","wcf1_");
+ */
+
+?>
