523f0e835bc8345f5f2dd716127091bcfd53fa9d
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2011-01-02 14:40:37 +0000

    Hehe.
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@91 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- update.php
+++ update.php
@@ -7,11 +7,26 @@ $version = "0.2";
         <title>Update</title>
     </head>
     <body>
-	<?php
-	    $xml = simplexml_load_file("http://twbbler.googlecode.com/svn/trunk/update.xml");
-	    if($xml->version < $version){
-		echo "Update verfügbar!";
-	    }
-	?>
+	<div>
+	    <?php
+		$xml = simplexml_load_file("http://twbbler.googlecode.com/svn/trunk/update.xml");
+		if($xml->version < $version){
+		    echo "Update verfügbar!<br>";
+		    $xml->update_files;
+		    $zip = new ZipArchive;
+		    if ($zip->open("http://twbbler.googlecode.com/svn/trunk/".$xml->version.".zip") === TRUE) {
+			$zip->extractTo(__DIR__.'/');
+			$zip->close();
+			echo 'Geupdatet!<br>';
+			echo 'Notes: '.$xml->note;
+		    } else {
+			echo 'Failed to update!<br>';
+			echo $zip->getStatusString;
+		    }
+		}else{
+		    echo "Kein Update verfügbar";
+		}
+	    ?>
+	</div>
     </body>
 </html>
