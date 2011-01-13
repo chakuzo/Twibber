af48de6cbd1eaade756ce79f5dac13f0830b8ef3
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2011-01-13 19:59:14 +0000

    Some Updates ty @ _MaX_ for signature code ! changed update.xml
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@155 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
 include("./class/Twibber.class.php");
-$return = wcf::getLoginOK($_COOKIE['twibber_nick'], $_COOKIE['twibber_pw'], $_COOKIE['twibber_salt']);
-$text = $_POST['text'];
 $nick = strip_tags($_COOKIE['twibber_nick']);
+$return = wcf::getLoginOK($nick, $_COOKIE['twibber_pw'], $_COOKIE['twibber_salt']);
+$text = $_POST['text'];
 if($_GET['new_entry'] == "1" && $return){
     if(trim($text) != "" && strlen($text) <= 250 && $nick != ""){
 	$Twibber->createTwibber(htmlspecialchars($text), htmlspecialchars($nick));
@@ -26,22 +26,31 @@ if(trim($_GET['search']) != ""){
 }
 if(trim($_GET['image']) != ""){
     Header("Content-type: image/png");
-    $nick = strip_tags($_GET['image']);
-    $im = @ImageCreate (468, 60)
-      or die ("Kann keinen neuen GD-Bild-Stream erzeugen");
-    $avatar = imagecreatefrompng("http://www.wbblite2.de/wcf/images/avatars/avatar-328.png"); 
+    $nick = wcf::getAvatar(strip_tags($_GET['image']));
+    $img = @ImageCreateTrueColor (468, 60)
+      or die ("Kann keinen neuen GD-Bild-Stream erzeugen"); 
     $new_width = 60;
     $new_height = 60;
     //$im = imagecreatefrompng("images/button1.png");
-    $background_color = ImageColorAllocate ($im, 0, 0, 0);
-    $text_color = ImageColorAllocate ($im, 233, 14, 91);
-    ImageStringUp($im, 2, 0, 55, "Twibber", $text_color);
-    ImageStringUp($im, 2, 5, 55, "_________", $text_color);
-    ImageString($im, 5, 80, 0, $nick."'s letzter Twib:", $text_color);
-    ImageString($im, 4, 80, 15, '--> '.$Twibber->fetchTwibber(true, false, $nick, 0, 30, true).'', $text_color);
-    ImageCopyresampled($im, $avatar, 20, 0, 0, 0, $new_width, $new_height, 468, 60);
-    ImagePNG($im);
-    ImageDestroy($im);
+    $background_color = ImageColorAllocate ($img, 0, 0, 0);
+    $text_color = ImageColorAllocate ($img, 233, 14, 91);
+    ImageStringUp($img, 2, 0, 55, "Twibber", $text_color);
+    ImageStringUp($img, 2, 5, 55, "_________", $text_color);
+    ImageString($img, 5, 80, 0, $nick."'s letzter Twib:", $text_color);
+    ImageString($img, 4, 80, 15, '--> '.$Twibber->fetchTwibber(true, false, $nick, 0, 30, true).'', $text_color);
+    $image_data = getimagesize($nick);
+    if ($image_data['mime'] == 'image/png') { // ty _MaX_
+	$avatar = imagecreatefrompng($nick);
+	imagecopyresampled($img, $avatar, 20, 0, 0, 0, 60, 60, imagesx($avatar), imagesy($avatar));
+    } elseif ($image_data['mime'] == 'image/jpeg' or $image_data['mime'] == 'image/pjpeg') {
+	$avatar = imagecreatefromjpeg($nick);
+        imagecopyresampled($img, $avatar, 20, 0, 0, 0, 60, 60, imagesx($avatar), imagesy($avatar));
+    } elseif ($image_data['mime'] == 'image/gif') {
+        $avatar = imagecreatefromgif($nick);
+        imagecopyresampled($img, $avatar, 20, 0, 0, 0, 60, 60, imagesx($avatar), imagesy($avatar));
+    }
+    ImagePNG($img);
+    ImageDestroy($img);
     ImageDestory($avatar);
     exit();
 }
