f798817b5156cc4a771888feddc020733463971e
kurtextrem <kurtextrem@1a2ef0f9-509c-9fab-45e8-d1e93232252b>
2011-01-13 15:41:20 +0000

    RegExp from "Clickable Links" a fantasctic Chrome Extension, which i use now 1 year or longer =)
    git-svn-id: http://twbbler.googlecode.com/svn/trunk@148 1a2ef0f9-509c-9fab-45e8-d1e93232252b

--- class/Twibber.class.php
+++ class/Twibber.class.php
@@ -104,7 +104,7 @@ class Twibber {
     }
     function replace_text($text){
 	$text = str_replace("\\","",$text);
-	$text = preg_replace('~((https?://|www\.)([-\w\.]+)+(:\d+)?(/([-]*)?)?)~i','<a href="$1">$1</a>',$text);
+	$text = preg_replace('/((?:https?|ftp):\/\/[^\s\'"\'<>()]+|www\.[^\s\'"\'<>()]+|[\-\w.+]+@(?:[\-\w]+\.)+[\w]{2,6})/i','<a href="$1">$1</a>',$text);
 	$text = preg_replace('/@(.+)/','<a href="?nick=$1" onclick="return dyn_get(true, false, this.innerText.replace(/@/,\'\'));">@$1</a> ',$text);
 	$text = preg_replace('/#(.+)/','<a href="?search=$1" class="hash" onclick="return dyn_get(true, false, false, this.innerText.replace(~#~,\'\'));">#$1</a> ',$text);
 	return $text;
