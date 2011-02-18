8df82e2bbfb76a10f61ae8fa3206a7823a961b65
TimWolla <timwolla@arcor.de>
2011-02-19 01:36:54 +0800

    Functions lowercase

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ if($_GET['mode'] == 'logout'){
     setcookie('twibber_nick', '', time() - 3600);
     setcookie('twibber_pw', '', time() - 3600);
     setcookie('twibber_salt', '', time() - 3600);
-    Header('Location: index.php');
+    header('Location: index.php');
 }
 if(!empty($_POST['nickname']) && !empty($_POST['password'])){
     $return = wcf::getData($_POST['nickname'], $_POST['password']);
@@ -12,7 +12,7 @@ if(!empty($_POST['nickname']) && !empty($_POST['password'])){
 	setcookie('twibber_nick', $_POST['nickname'], time()+(365 * 24 * 60 * 60));
 	setcookie('twibber_pw', sha1($_POST['password']), time()+(365 * 24 * 60 * 60));
 	setcookie('twibber_salt', wcf::getSalt($_POST['nickname']), time()+(365 * 24 * 60 * 60));
-	Header('Location: index.php');
+	header('Location: index.php');
     }else{
 	echo $lang_false_pw_nick;
     }
