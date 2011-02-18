2030b7bb2f0cac04e4498f2b94a72a5214338fac
TimWolla <timwolla@arcor.de>
2011-02-18 18:25:35 +0100

    Required claaes

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php
-include("lib/class/Twibber.class.php");
-if($_GET['mode'] == "logout"){
-    setcookie("twibber_nick", "", time() - 3600);
-    setcookie("twibber_pw", "", time() - 3600);
-    setcookie("twibber_salt", "", time() - 3600);
-    Header("Location: index.php");
+require_once('lib/class/Twibber.class.php');
+if($_GET['mode'] == 'logout'){
+    setcookie('twibber_nick', '', time() - 3600);
+    setcookie('twibber_pw', '', time() - 3600);
+    setcookie('twibber_salt', '', time() - 3600);
+    Header('Location: index.php');
 }
-if($_POST['nickname'] != "" && $_POST['password'] != ""){
+if(!empty($_POST['nickname']) && !empty($_POST['password'])){
     $return = wcf::getData($_POST['nickname'], $_POST['password']);
     if($return){
-	setcookie("twibber_nick", $_POST['nickname'], time()+(365 * 24 * 60 * 60));
-	setcookie("twibber_pw", sha1($_POST['password']), time()+(365 * 24 * 60 * 60));
-	setcookie("twibber_salt", wcf::getSalt($_POST['nickname']), time()+(365 * 24 * 60 * 60));
-	Header("Location: index.php");
+	setcookie('twibber_nick', $_POST['nickname'], time()+(365 * 24 * 60 * 60));
+	setcookie('twibber_pw', sha1($_POST['password']), time()+(365 * 24 * 60 * 60));
+	setcookie('twibber_salt', wcf::getSalt($_POST['nickname']), time()+(365 * 24 * 60 * 60));
+	Header('Location: index.php');
     }else{
 	echo $lang_false_pw_nick;
     }
