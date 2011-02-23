8eb10326b3e001a106e1a5872deefeb32e822c32
TimWolla <timwolla@arcor.de>
2011-02-23 20:01:01 +0100

    There should be exit after redirect

--- user/login.php
+++ user/login.php
@@ -6,6 +6,7 @@ if (isset($_GET['mode']) && $_GET['mode'] == 'logout') {
 	setcookie('twibber_pw', '', time() - 3600);
 	setcookie('twibber_salt', '', time() - 3600);
 	header('Location: ../index.php');
+	exit;
 }
 if (!empty($_POST['nickname']) && !empty($_POST['password'])) {
 	$return = WCF::getData($_POST['nickname'], $_POST['password']);
@@ -14,6 +15,7 @@ if (!empty($_POST['nickname']) && !empty($_POST['password'])) {
 		setcookie('twibber_pw', sha1($_POST['password']), time() + (365 * 24 * 60 * 60));
 		setcookie('twibber_salt', WCF::getSalt($_POST['nickname']), time() + (365 * 24 * 60 * 60));
 		header('Location: ../index.php');
+		exit;
 	} else {
 		echo $lang_false_pw_nick;
 	}
